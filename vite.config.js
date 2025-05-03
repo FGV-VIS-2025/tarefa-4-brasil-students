45c84ec2b77ee81a19a940dce94867575dd80408
Leighton Estrada <lestradar@uni.edu.pe>
2025-05-03 12:47:17 -0300

    Update01

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/tarefa-4-brasil-students/',
   server: {
     port: 3000,
     open: true
